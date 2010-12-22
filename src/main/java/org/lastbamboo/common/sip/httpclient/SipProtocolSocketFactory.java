83dc331b258ff1bdbcfb8f422ef11ae00488587e
adamfisk <a@littleshoot.org>
2010-12-22 18:30:48 +0000

    Lots of refactorings to clean up TCP socket resolution and creation
    git-svn-id: http://svn.littleshoot.org/svn/littleshoot/trunk/common/sip/http-client@7679 8d7aff80-242d-0410-bcee-ed6f15385b3d

--- src/main/java/org/lastbamboo/common/sip/httpclient/SipProtocolSocketFactory.java
+++ src/main/java/org/lastbamboo/common/sip/httpclient/SipProtocolSocketFactory.java
@@ -10,16 +10,17 @@ import java.util.prefs.Preferences;
 import org.apache.commons.httpclient.ConnectTimeoutException;
 import org.apache.commons.httpclient.params.HttpConnectionParams;
 import org.apache.commons.httpclient.protocol.ProtocolSocketFactory;
+import org.apache.commons.io.IOExceptionWithCause;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.lastbamboo.common.offer.answer.NoAnswerException;
+import org.lastbamboo.common.p2p.SocketFactory;
 import org.lastbamboo.common.sip.stack.SipUriFactory;
-import org.lastbamboo.common.util.SocketFactory;
 
 /**
  * Socket factory for creating SIP-negotiated sockets for HTTP clients.
  */
-public final class SipProtocolSocketFactory implements ProtocolSocketFactory
-    {
+public final class SipProtocolSocketFactory implements ProtocolSocketFactory {
 
     /**
      * Logger for this class.
@@ -39,58 +40,53 @@ public final class SipProtocolSocketFactory implements ProtocolSocketFactory
      * names from HTTP client.
      */
     public SipProtocolSocketFactory(final SocketFactory sipSocketFactory,
-        final SipUriFactory sipUriFactory)
-        {
+            final SipUriFactory sipUriFactory) {
         this.m_sipSocketFactory = sipSocketFactory;
         this.m_sipUriFactory = sipUriFactory;
-        }
+    }
 
-    public Socket createSocket(final String host, final int port, 
-        final InetAddress clientHost, final int clientPort)
-        {
+    public Socket createSocket(final String host, final int port,
+            final InetAddress clientHost, final int clientPort) {
         m_log.warn("Attempted unsupported socket call");
         throw new UnsupportedOperationException("not allowed");
-        }
+    }
 
-    public Socket createSocket(final String host, final int port, 
-        final InetAddress clientHost, final int clientPort, 
-        final HttpConnectionParams params) throws IOException, 
-        UnknownHostException, ConnectTimeoutException
-        {
+    public Socket createSocket(final String host, final int port,
+            final InetAddress clientHost, final int clientPort,
+            final HttpConnectionParams params) throws IOException,
+            UnknownHostException, ConnectTimeoutException {
         m_log.trace("Creating a socket for user: {}", host);
         return createSocket(host, port);
-        }
+    }
     
-    public Socket createSocket(final String host, final int port) 
-        throws IOException, UnknownHostException
-        {
+    public Socket createSocket(final String host, final int port)
+            throws IOException, UnknownHostException {
         m_log.trace("Creating a socket for user: {}", host);
         final Preferences prefs = Preferences.userRoot();
         final long id = prefs.getLong("LITTLESHOOT_ID", -1);
-        if (id == Long.parseLong(host))
-            {
+        if (id == Long.parseLong(host)) {
             // This is an error because we should just stream it locally
             // if we have the file (we're trying to connect to ourselves!).
             m_log.error("Ignoring request to download from ourselves...");
             throw new IOException("Not downloading from ourselves...");
-            }
-        
+        }
+
         final URI sipUri = this.m_sipUriFactory.createSipUri(host);
-        try 
-            {
+        try {
             m_log.trace("About to create socket...");
             final Socket sock = this.m_sipSocketFactory.newSocket(sipUri);
             m_log.debug("Got socket!! Returning to HttpClient");
-            
+
             // Note there can appear to be an odd delay after this point if
             // you're just looking at the raw logs, but it's due to HttpClient
             // actually making the HTTP request and getting a response.
             return sock;
-            }
-        catch (final IOException e)
-            {
+        } catch (final NoAnswerException e) {
+            m_log.warn("No answer!!", e);
+            throw e;
+        } catch (final IOException e) {
             m_log.warn("Exception creating SIP socket", e);
             throw e;
-            }
         }
     }
+}
